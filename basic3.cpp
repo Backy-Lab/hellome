962d4fd81506bb131abbb327e802c1b2c0975fa2
Backy <kartikshahu120@gamil.com>
2025-10-15 20:54:43 +0530

    gg

---
+++ basic3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a,b,c;
+cout<<"Enter the first Number :";
+cin>>a;
+cout<<endl<<"Enter the second Number :";
+cin>>b;
+c=a+b;
+cout<<"the sum is = "c <<endl;
+return 0;
+};
