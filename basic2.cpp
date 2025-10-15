e90caef1199cc9d25436c9e50c59447f4187d22b
Backy <kartikshahu120@gamil.com>
2025-10-15 20:52:10 +0530

    gg

---
+++ basic2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Subject \t Marks"<<endl;
+cout<<"Mathematics \t 90"<<endl;
+cout<<"Computer \t 77"<<endl;
+cout<<"Chemistry \t 69"<<endl;
+};
