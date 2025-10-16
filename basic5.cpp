92667eaa210d5e688c3c8630b34d37b09214c1cf
Backy <kartikshahu120@gamil.com>
2025-10-16 22:00:14 +0530

    gg

---
+++ basic5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a,c;
+cout<<"Enter temperature in Farenheit ";
+cin>>a;
+c=(32*a-32)*5/9;
+cout<<"Temperature in celcius is :"<<c <<endl;
+return 0;
+}
